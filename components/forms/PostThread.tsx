c897f3f141b3eb27c66116f339437331a2b2e6d2
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-04-11 20:29:46 -0400

    Create Thread page FormField

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,5 +1,74 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { ThreadValidation } from "@/lib/validations/thread";
+import { Textarea } from "../ui/textarea";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    email: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
 function PostThread({ userId }: { userId: string }) {
-  return <h1>Post Thread Form</h1>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-bold-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Textarea rows={15} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
 
 export default PostThread;
