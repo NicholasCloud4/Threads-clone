3f24a47a244006818f2fb48cbd01c2fab54047c3
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-04-12 11:16:38 -0400

    Thread Card Structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
+
+  console.log(result);
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((posts) => (
+              <ThreadCard
+                key={posts._id}
+                id={posts._id}
+                currentUserId={user?.id || ""}
+                parentId={posts.parentId}
+                content={posts.text}
+                author={posts.author}
+                community={posts.community}
+                createdAt={posts.createdAt}
+                comments={posts.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import path from "path";
 
 interface Params {
   text: string;
@@ -35,4 +36,36 @@ export async function createThread({
   } catch (error: any) {
     throw new Error(`Failed to create thread: ${error.message}`);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch the posts that have no parents (TOP-LEVEL)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: "user",
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
