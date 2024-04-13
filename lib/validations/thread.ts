db1e37b7e2eade34a027c45bc7d3b1ddf97c306e
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-04-13 11:48:37 -0400

    Completed Thread Card Structure

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -1,18 +1,10 @@
 import * as z from "zod";
 
 export const ThreadValidation = z.object({
-  thread: z
-    .string()
-    .nonempty()
-    .min(3, { message: "Minimum 3 characters." })
-    .max(30, { message: "Maximum 30 caracters." }),
+  thread: z.string().nonempty().min(3, { message: "Minimum 3 characters." }),
   accountId: z.string(),
 });
 
 export const CommentValidation = z.object({
-  thread: z
-    .string()
-    .nonempty()
-    .min(3, { message: "Minimum 3 characters." })
-    .max(30, { message: "Maximum 30 caracters." }),
+  thread: z.string().nonempty().min(3, { message: "Minimum 3 characters." }),
 });
